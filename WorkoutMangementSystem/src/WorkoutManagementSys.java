106cdb5bb645933aeea0de461788e711c534db64
codthestudent <hjun303@gnu.ac.kr>
2022-03-13 17:05:47 +0900

    add menu
    added menu for the workout management project

---
+++ WorkoutMangementSystem/src/WorkoutManagementSys.java
@@ -0,0 +1,14 @@
+
+public class WorkoutManagementSys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. Add Workout days");
+		System.out.println("2. Delete Workout days");
+		System.out.println("3. Edit Workout day");
+		System.out.println("4. View total Workout days");
+		System.out.println("5. Exit");
+
+	}
+
+}
